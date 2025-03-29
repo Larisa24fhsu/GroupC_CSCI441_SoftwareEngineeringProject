65e027e581407c35ce7ffd83570a86f431339619
Larisa <lbsmith2@mail.fhsu.edu>
2025-03-28 21:25:24 -0400

    connected Postgres on Render

---
+++ db.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,  // Required for Render-hosted PostgreSQL
+  }
+});
+
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL on Render!"))
+  .catch(err => console.error("Connection error", err));
+
+module.exports = pool;
