325556ff80b59fd44ec2ae97914f3d4e040207fa
Pavel Martinez <pavomartinez@hotmail.com>
2025-04-02 15:07:42 -0700

    Made adjustmentts to the 404 redirect

--- server.js
+++ server.js
@@ -124,8 +124,15 @@ app.get("^/$|/index(.html)?", (req, res) => {
 });
 
 //Redirect all incorrect traffic to a 404.html page
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
 });
 
 //CORS Error handling
