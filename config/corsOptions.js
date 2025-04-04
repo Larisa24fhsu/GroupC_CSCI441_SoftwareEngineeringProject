dd25cbb3c8a87abb8c1d57f4c19c355f3a341ab8
Larisa <lbsmith2@mail.fhsu.edu>
2025-04-03 21:15:46 -0400

    added POST in html/js for user interface

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -6,6 +6,7 @@ const whitelist = [
   "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
+  "http://localhost:3444"
 ];
 const corsOptions = {
   origin: (origin, callback) => {
