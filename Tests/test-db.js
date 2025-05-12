a7d3964ee192da21d29ef6dd0580463b5562e448
Larisa24fhsu <lbsmith2@mail.fhsu.edu>
2025-05-11 21:13:26 -0400

    update electronic archive

---
+++ Tests/test-db.js
@@ -0,0 +1,17 @@
+//originally added just to test back end, not used for actual project
+//written by Larisa Smith
+
+const pool = require('./db');
+
+async function testQuery() {
+  try {
+    const res = await pool.query('SELECT NOW()');
+    console.log('Database Time:', res.rows[0].now);
+  } catch (err) {
+    console.error('Error executing query', err);
+  } finally {
+    pool.end();
+  }
+}
+
+testQuery();
